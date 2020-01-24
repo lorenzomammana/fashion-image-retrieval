64a51a1c4dff1f094ab696650d3be11db4583ee6
lorenzomammana <lorenzom96@hotmail.it>
2020-01-24 15:24:17 +0100

    Train with labels in random order

--- deepranking/generate_clusters.py
+++ deepranking/generate_clusters.py
@@ -10,13 +10,18 @@ import numpy as np
 import os
 import shutil
 import joblib
+from keras.applications.vgg16 import preprocess_input
+from tqdm import tqdm
 
 if __name__ == '__main__':
-    
     model = FashionRankingModel().compile(weights=files.deepranking_weights_path)
 
     classes_dirs = [d for d in files.small_images_classes_directory.iterdir() if d.is_dir()]
     classes_names = [d.parts[-1] for d in classes_dirs]
+
+    images = []
+    for p, c in zip(classes_dirs, classes_names):
+        images = images + [i.as_posix().split('/')[-2:] for i in p.iterdir() if i.is_file()]
     n_classes = len(classes_names)
     embedding_values = []
 
@@ -24,43 +29,34 @@ if __name__ == '__main__':
     kmeans = MiniBatchKMeans(n_clusters=n_classes, random_state=0, batch_size=batch_size)
 
     # For each class
-    for p, c in zip(classes_dirs, classes_names):
+    images = np.random.permutation(images)
 
-        images = [i for i in p.iterdir() if i.is_file()]
-        images_names = [i.stem for i in images]
-        
-        embedding_dir = files.small_images_classes_embeddings / c
-
-        shutil.rmtree(embedding_dir, ignore_errors=True)
-        os.mkdir(embedding_dir)
+    for label, i in tqdm(images):
 
         # Compute embedding value and save to txt file
-        for i, n in zip(images, images_names):
+        img = load_img(files.small_images_classes_directory / label / i, target_size=(224, 224))
+        img = img_to_array(img)
+        img = preprocess_input(img)
+        img = np.expand_dims(img, axis=0)
 
-            img = load_img(i)
-            img = img_to_array(img).astype('float64')
-            img = transform.resize(img, (224, 224))
-            img *= 1. / 255
-            img = np.expand_dims(img, axis=0)
-            
-            ev = model.predict([img, img, img])[0]
-            embedding_values.append(ev)
-            np.savetxt(embedding_dir / '{}.txt'.format(n), ev)
-
-            # Update kmeans
-            if len(embedding_values) % batch_size == 0:
-
-                kmeans.partial_fit(np.array(embedding_values))
-                embedding_values = []
+        ev = model.predict([img, img, img])[0]
+        embedding_values.append(ev)
+        np.savetxt(files.small_images_classes_embeddings / label / i.replace('jpg', 'txt'), ev)
 
         # Update kmeans
-        if len(embedding_values) > 0:
-            
+        if len(embedding_values) % batch_size == 0:
             kmeans.partial_fit(np.array(embedding_values))
             embedding_values = []
 
+    # Update kmeans
+    if len(embedding_values) > 0:
+        kmeans.partial_fit(np.array(embedding_values))
+        embedding_values = []
+
     # Save kmeans object to file
     joblib.dump(kmeans, files.small_images_classes_kmeans)
+
+    # kmeans = joblib.load(files.small_images_classes_kmeans)
 
     # Assign each cluster to a specific class
     centroids_class_frequency = []
@@ -72,17 +68,21 @@ if __name__ == '__main__':
 
         centroids_class_frequency.append(class_frequency)
 
+    file = open(files.small_images_classes_embeddings / 'features.csv', 'w')
+
     for c in classes_names:
 
         embedding_dir = files.small_images_classes_embeddings / c
         embedding_files = [v for v in embedding_dir.iterdir() if v.is_file()]
 
         for f in embedding_files:
-
             embedding = np.loadtxt(f)
             prediction = kmeans.predict(np.array([embedding]))[0]
 
+            file.write('/'.join(f.as_posix().split('/')[-2:]) + ',' + str(prediction) + ',' + c + '\n')
             centroids_class_frequency[prediction][c] += 1
+
+    file.close()
 
     centroids_classes = []
     for i in range(len(centroids_class_frequency)):
@@ -90,7 +90,7 @@ if __name__ == '__main__':
         max_frequency = (None, -1)
 
         for k, v in centroids_class_frequency[i].items():
-            
+
             if v > max_frequency[1]:
                 max_frequency = (k, v)
 
