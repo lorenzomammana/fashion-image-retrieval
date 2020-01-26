78403d963c21522257749f27975e39bbf667e5c7
lorenzomammana <lorenzom96@hotmail.it>
2020-01-26 09:37:20 +0100

    Update similarity using resnet model

--- deepranking/fashion_similarity.py
+++ deepranking/fashion_similarity.py
@@ -1,42 +1,38 @@
-import fashion_utils
 import files
-import fashion_dataset
-from fashion_ranking_model import FashionRankingModel
-from sklearn.cluster import MiniBatchKMeans
 from keras.preprocessing.image import load_img, img_to_array
 import pandas as pd
-from skimage import transform
 import numpy as np
-import os
-import shutil
 import joblib
 from keras.applications.vgg16 import preprocess_input
+from keras.models import load_model
+from fashion_utils import loss_tensor
+from keras_applications.resnext import preprocess_input
+import keras
 
-class FashionSimilarity():
+
+class FashionSimilarity:
 
     def __init__(self):
-
-        self.model = FashionRankingModel().compile(weights=files.deepranking_weights_path)
+        self.model = load_model(files.deepranking_weights_path.absolute().as_posix(),
+                                custom_objects={'loss_tensor': loss_tensor})
         self.kmeans = joblib.load(files.small_images_classes_kmeans)
         self.centroid_classes = pd.read_csv(files.small_images_classes_centroids)
         self.embeddings = pd.read_csv(files.small_images_classes_features)
 
     def get_similar_images(self, img_path, n):
-
         img = load_img(img_path, target_size=(224, 224))
         img = img_to_array(img)
-        img = preprocess_input(img)
+        img = preprocess_input(img, backend=keras.backend, layers=keras.layers, models=keras.models, utils=keras.utils)
         img = np.expand_dims(img, axis=0)
 
-        ev = self.model.predict([img, img, img])[0]
-        centroid = self.kmeans.predict(np.array([ev]))[0]
+        ev, label = self.model.predict([img, img, img])
+        centroid = self.kmeans.predict(ev)[0]
 
         predicted_class = self.centroid_classes[self.centroid_classes['centroid'] == centroid].iloc[0, 1]
         cluster_images = self.embeddings[self.embeddings['cluster'] == centroid]
 
         similarity_scores = []
         for i in range(cluster_images.shape[0]):
-            
             ev_id = cluster_images.iloc[i, 0]
             ev_class = cluster_images.iloc[i, 2]
             ev_path = files.small_images_classes_embeddings / ev_class / '{}.txt'.format(ev_id)
