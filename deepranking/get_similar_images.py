7a20f50a2e4377370a2fbc45b12dfc98ec0b4d88
lorenzomammana <lorenzom96@hotmail.it>
2020-02-04 16:49:57 +0100

    Add empty package file

--- deepranking/get_similar_images.py
+++ deepranking/get_similar_images.py
@@ -8,24 +8,31 @@ from fashion_similarity_online import FashionSimilarity
 import numpy as np
 import matplotlib.pyplot as plt
 from keras_preprocessing.image import load_img, img_to_array
+from matplotlib import gridspec
 
 
-def plot_output_image(label, clothes):
-    plt.clf()
-    fig = plt.figure(figsize=(5, 2))
-    columns = 5
-    rows = 2
-
-    for i in range(1, columns * rows + 1):
-        filename = similar_images['class'].iloc[i - 1] + '/' + str(similar_images['id'].iloc[i - 1]) + '.jpg'
-        img = load_img((files.small_images_classes_directory / filename).absolute().as_posix())
-        img = img_to_array(img) / 255
-        fig.add_subplot(rows, columns, i)
-        plt.axis('off')
-        plt.imshow(img)
+def plot_output_image(query, label, clothes):
+    gs00 = gridspec.GridSpec(1, 2, width_ratios=[1, 2])
+    gs01 = gridspec.GridSpecFromSubplotSpec(2, 5, subplot_spec=gs00[0, 1])
+    fig = plt.figure(figsize=(8, 3))
+    left_ax = fig.add_subplot(gs00[0, 0])
+    plt.axis('off')
+    input = load_img(query)
+    input = img_to_array(input) / 255
+    plt.imshow(input)
+    for i in range(2):
+        for j in range(5):
+            filename = similar_images['class'].iloc[5 * i + j] + '/' + \
+                       str(similar_images['id'].iloc[5 * i + j]) + '.jpg'
+            print(filename)
+            img = load_img((files.small_images_classes_directory / filename).absolute().as_posix())
+            img = img_to_array(img) / 255
+            fig.add_subplot(gs01[i, j])
+            plt.axis('off')
+            plt.imshow(img)
 
     plt.savefig((files.ROOT / 'similarity-output' / label / ('out_' + clothes)).absolute().as_posix())
-    plt.close()
+    plt.clf()
 
 
 if __name__ == '__main__':
@@ -66,4 +73,4 @@ if __name__ == '__main__':
             print(fname)
             print("Predicted class: " + idx_to_class.get(c))
             print(similar_images)
-            plot_output_image(label, fname)
+            plot_output_image(query_img, label, fname)
