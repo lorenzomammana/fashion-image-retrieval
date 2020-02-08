98fe9f3be1499493443a1e455b69dcd308daab3c
lorenzomammana <lorenzom96@hotmail.it>
2020-02-08 12:35:16 +0100

    Minor fix

--- server_bot.py
+++ server_bot.py
@@ -1,5 +1,5 @@
 from bot.Updater import Updater
-import os, sys, platform, subprocess
+import os
 from deepranking import compute_single_image, FashionSimilarity, compute_single_image_lsh, FashionSimilarityLSH
 from maskrcnn_modanet import load_mask_rcnn, segment_image
 
@@ -8,6 +8,7 @@ mask_rcnn_model, labels_to_names = load_mask_rcnn()
 similarity_model = FashionSimilarity()
 # similarity_model = FashionSimilarityLSH()
 print('Models loaded.')
+
 
 def fileparts(fn):
     (dirName, fileName) = os.path.split(fn)
@@ -26,8 +27,9 @@ def imageHandler(bot, message, chat_id, local_filename):
 
     for i in range(num_images):
         compute_single_image(i, similarity_model)
-        # compute_single_image_lsh(i, similarity_model)
         bot.sendImage(chat_id, '/tmp/out_' + str(i) + '.jpg', "")
+        # compute_single_image_lsh(i, similarity_model)
+        # bot.sendImage(chat_id, '/tmp/out_lsh_' + str(i) + '.jpg', "")
 
 
 if __name__ == "__main__":
